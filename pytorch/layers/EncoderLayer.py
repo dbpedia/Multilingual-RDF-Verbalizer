b6da83b6e02c983223db42b77d2ed463452d20f3
Marco A. S. Cabezudo <msobrevillac@gmail.com>
2020-07-12 16:22:29 -0300

    Transformer-based S2S in pytorch

---
+++ pytorch/layers/EncoderLayer.py
@@ -0,0 +1,43 @@
+import torch.nn as nn
+from MultiHeadAttentionLayer import MultiHeadAttentionLayer
+from PositionwiseFeedforwardLayer import PositionwiseFeedforwardLayer
+
+class EncoderLayer(nn.Module):
+    def __init__(self, 
+                 hid_dim, 
+                 n_heads, 
+                 pf_dim,  
+                 dropout, 
+                 device):
+        super().__init__()
+        
+        self.self_attn_layer_norm = nn.LayerNorm(hid_dim)
+        self.ff_layer_norm = nn.LayerNorm(hid_dim)
+        self.self_attention = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, device)
+        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hid_dim, 
+                                                                     pf_dim, 
+                                                                     dropout)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, src, src_mask):
+        
+        #src = [batch size, src len, hid dim]
+        #src_mask = [batch size, src len]
+                
+        #self attention
+        _src, _ = self.self_attention(src, src, src, src_mask)
+        
+        #dropout, residual connection and layer norm
+        src = self.self_attn_layer_norm(src + self.dropout(_src))
+        
+        #src = [batch size, src len, hid dim]
+        
+        #positionwise feedforward
+        _src = self.positionwise_feedforward(src)
+        
+        #dropout, residual and layer norm
+        src = self.ff_layer_norm(src + self.dropout(_src))
+        
+        #src = [batch size, src len, hid dim]
+        
+        return src
