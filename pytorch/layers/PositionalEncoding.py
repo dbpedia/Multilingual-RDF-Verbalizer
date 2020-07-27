88304c33fda24770e998ba7a24c99a32f006add7
Marco A. S. Cabezudo <msobrevillac@gmail.com>
2020-07-27 18:00:59 -0300

    adding Positional Encoding

---
+++ pytorch/layers/PositionalEncoding.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, hid_dim, dropout, max_length=100):
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_length, hid_dim)
+        position = torch.arange(0, max_length).unsqueeze(1)
+
+        div_term = torch.exp(torch.arange(0, hid_dim, 2) *
+                             -(math.log(10000.0) / hid_dim))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+
+        x = x + Variable(self.pe[:, :x.size(1)],
+                         requires_grad=False)
+        #return self.dropout(x)
+        return x
