4cee99abfeebee94007bfaf39bad117a4664cc67
Marco A. S. Cabezudo <msobrevillac@gmail.com>
2020-08-07 20:08:15 -0300

    implementing beam search

--- pytorch/Translate.py
+++ pytorch/Translate.py
@@ -5,6 +5,7 @@ from queue import PriorityQueue
 
 def translate(model, task_id, sentences, source_vocab, target_vocab, device, max_length=180, beam_size=None):
 	outputs = []
+	print(beam_size)
 	if beam_size is None:
 		print("Using greedy search")
 		for sentence in sentences:
