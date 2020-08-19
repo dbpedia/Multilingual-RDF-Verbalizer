deda02dd7b7fd9e825286fba0b45d7f8ea99c507
Marco A. S. Cabezudo <msobrevillac@gmail.com>
2020-08-18 22:53:09 -0300

    add pipeline

--- pytorch/pipeline.sh
+++ pytorch/pipeline.sh
@@ -23,7 +23,7 @@ for set in dev test
 do
 mv /home/marcosbc/results/output.03082020/lexicalization/structuring.mapped.$set.0.out /home/marcosbc/results/pipeline/lexicalization.$set
 
-python /home/marcosbc/Multilingual-RDF-Verbalizer/pytorch/reg/generate.py /home/marcosbc/results/pipeline/lexicalization.$set /home/marcosbc/results/pipeline/ordering.mapped.$set neuralreg /home/marcosbc/reg_model/model1.dy
+python /home/marcosbc/Multilingual-RDF-Verbalizer/pytorch/utils/generate.py /home/marcosbc/results/pipeline/lexicalization.$set /home/marcosbc/results/pipeline/ordering.mapped.$set neuralreg /home/marcosbc/reg_model/model1.dy
 
 done
 
